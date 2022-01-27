be883a30ffa84a13ff0c8d40ec887889818c77b9
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-01-27 23:56:56 +0900

    Uploading  webpage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:react/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "eslint-config-prettier",
-        "plugin:react/recommended"
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier",
-    ],
-    "rules": {
-        "prettier/prettier": "error",
-    }
-}
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // searchKeyword: "",
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div>Hello JayMarket</div>
+      </>
+    );
+  }
+}
