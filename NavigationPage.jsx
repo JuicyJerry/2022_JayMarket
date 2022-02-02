88f29abebb8fea3a5318668bb2cac0507111e0b3
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-02-02 18:21:38 +0900

    Navigation page: UI & CSS

--- NavigationPage.jsx
+++ NavigationPage.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 const NavigationPage = () => {
 
-
-
   return (
-    <>
-      <nav>
-        <ul id="menu">
-          <li className="subPage">Home</li>
-          <li className="subPage">Login</li>
-          <li className="subPage">Donation</li>
-        </ul>
-      </nav>
-    </>
+    <nav id="nav">
+      <ul id="menu">
+        <li className="subPage">Home</li>
+        <li className="subPage">Login</li>
+        <li className="subPage">Donation</li>
+      </ul>
+    </nav>
   )
 }
 
