33793944572f3f76c5c6f9a854a0b0c83906f4e4
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-02-02 19:09:42 +0900

    landing page: UI & Skeleton, css

--- Client.jsx
+++ Client.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LandingPage from "./LandingPage.jsx";
+// import LoginPage from "./LoginPage.jsx";
 
 ReactDOM.render(<LandingPage />, document.querySelector("#root"));
--- LandingPage.jsx
+++ LandingPage.jsx
@@ -1,44 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import NavigationPage from "./NavigationPage.jsx";
 
-export default class LandingPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      // ...
-    }
-  }
+const LandingPage = () => {
 
-  render() {
-    console.log("렌더링");
+  console.log("렌더링");
 
-    return (
-      <React.Fragment>
-        <header>
-          <div>제이마켓에 환영합니다</div>
-        </header>
-
-        <nav>
-          <ul id="menu">
-            <li className="subPage">Home</li>
-            <li className="subPage">Login</li>
-            <li className="subPage">Donation</li>
-          </ul>
-        </nav>
-        <article>
+  return (
+    <>
+      <NavigationPage />
+      <article id="landingMotherWrap">
+        <article id="searchWrap">
           <form>
             <input type="text" placeholder="검색어를 입력하세요" />
-            <button type="reset" value="X"></button>
-            <button>입력</button>
+            <button type="reset">X</button>
+            {/* <button>입력</button> */}
           </form>
 
-          <section>
-            <p>
-              결과을 기다려주세요
-            </p>
-          </section>
         </article>
-      </React.Fragment>
-    )
-  }
+        <article id="resultWrap">
+          <p>
+            결과을 기다려주세요
+          </p>
+        </article>
+      </article>
+    </>
+  )
 }
+
+// module.exports = LandingPage;
+export default LandingPage;
