8d73dc43fb12778676f5d235d1f454fa440a84d1
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-02-02 18:21:02 +0900

    LoginPage: UI & CSS

--- LoginPage.jsx
+++ LoginPage.jsx
@@ -4,23 +4,39 @@ import NavigationPage from "./NavigationPage.jsx";
 const LoginPage = () => {
   const [id, setId] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [value, setValue] = React.useState("");
+
+  const ClearInputText = (e) => {
+
+    setValue(e.currentTarget.value);
+  }
 
   return (
+
     <>
       <NavigationPage />
-      <header>로그인</header>
-      <article>
-        <form>
-          <input type="text" placeholder="ID를 입력하세요" />
-          <button type="reset" value="X"></button>
-          <input type="text" placeholder="Password를 입력하세요" />
-          <button type="reset" value="X"></button>
-          <button type="submit" value="로그인"></button>
-        </form >
-        <form>
+      <header id="header"><h1>로그인</h1></header>
+
+      <article id="loginWrap">
+
+        <section>
+          <form className="loginForm">
+            <input type="text" placeholder="ID를 입력하세요" />
+            <button className="clearBtn" onClick={ClearInputText}>X</button>
+          </form>
+          <form className="loginForm">
+            <input type="text" placeholder="Password를 입력하세요" />
+            <button className="clearBtn" onClick={ClearInputText}>X</button>
+          </form>
+        </section>
+
+        <section id="loginBtn">
+          <button type="submit">로그인</button>
           <button type="button">Google</button>
           <button type="button">Kakao</button>
-        </form>
+        </section>
+
+
       </article >
     </>
   )
