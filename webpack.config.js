08461ca1c61684c3c2ffefb5d68f9ac11e9f9fbe
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-02-02 18:24:31 +0900

    Nav & Login Page: css, skeleton, webpack setting(hot reloading etc)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   name: "JayMarket",
@@ -12,12 +13,33 @@ module.exports = {
       test: /\.jsx?/,
       loader: "babel-loader",
       options: {
-        presets: ["@babel/preset-env", "@babel/preset-react"],
-      }
+        presets: [
+          ["@babel/preset-env", {
+            targets: {
+              browsers: ["> 5% in KR", "last 2 chrome versions"]
+            },
+          }], "@babel/preset-react"],
+        plugins: [
+          "react-refresh/babel",
+        ]
+      },
     }]
   },
+  plugins: [
+    // new webpack.LoaderOptionsPlugin({ debug: true }),
+    new RefreshWebpackPlugin()
+  ],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    // publicPath: "/dist/",
+
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+
   }
 }
